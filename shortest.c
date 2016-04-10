62a4453d7561e50bf973072cb4cb84527dd763f0
Neelo <neelo_sim@hotmail.com>
2016-04-10 15:19:35 +0100

    Added Floyd-Warshall algorithm for finding shortest paths between all pairs

--- shortest.c
+++ shortest.c
@@ -37,6 +37,32 @@ void print_graph(Vertex *graph, int size) {
     }
 }
 
+int *get_shortest_all_pairs(int *weights, int size) {
+    int i, j, k;
+    int *d = weights;
+    for (k = 0; k < size; k++)
+        for (i = 0; i < size; i++)
+            for (j = 0; j < size; j++) {
+                if (d[i*size + k] == INF || d[k*size + j] == INF) /* Let's avoid an overflow */
+                    continue;
+                d[i*size + j] = MIN(d[i*size + j], d[i*size + k] + d[k*size + j]);
+            }
+    return d;
+}
+
+void print_matrix(int *matrix, int size) {
+    int i, j;
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            if (matrix[i*size + j] == INF)
+                printf("%s", "| INF ");
+            else
+                printf("| %d ", matrix[i*size + j]);
+        }
+        puts("|");
+    }
+}
+
 int main(int argc, const char *argv[])
 {
     int num_places = 0, num_branches = 0, num_connections = 0;
